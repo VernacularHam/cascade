e5ca12a4f8cc8a1b7fd0d3c780d0b00d607cba81
Pratap Gajjala <pratap@formation-i.com>
2016-07-24 17:35:34 -0500

    Baseline commit

---
+++ src/Components/HamCanvas.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './HamCanvas.css';
+
+class HamCanvas extends Component {
+  render() {
+    return (
+      <div className="HamCanvas">
+        <canvas></canvas>
+      </div>
+    );
+  }
+}
+
+export default HamCanvas;
